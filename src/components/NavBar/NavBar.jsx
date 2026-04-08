2cf799b49d199584f5b21af57c982cd29fabc27e
jayedHoshen <jayedkn64@gmail.com>
2026-04-08 19:51:17 +0600

    initialize react with tailwind css

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import Link from "./Link";
+import { Menu, X } from "lucide-react";
+
+const navLinks = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    name: "About",
+    path: "/about",
+  },
+  {
+    id: 3,
+    name: "Services",
+    path: "/services",
+  },
+  {
+    id: 4,
+    name: "Blog",
+    path: "/blog",
+  },
+  {
+    id: 5,
+    name: "Contact",
+    path: "/contact",
+  },
+];
+
+const NavBar = () => {
+  const [open, setOpen] = useState(false);
+
+  const links = navLinks.map((route) => (
+    <Link key={route.id} route={route}></Link>
+  ));
+
+  return (
+    <nav className="flex justify-between mx-10">
+      <span className="flex" onClick={() => setOpen(!open)}>
+        <span className="md:hidden">{open ? <X></X> : <Menu></Menu>}</span>
+        <h3 className="ml-4">My Navbar</h3>
+      </span>
+      {/* 3. component navbar */}
+      <ul className="flex">{links}</ul>
+
+      {/* 2. dynamic navbar */}
+      {/* <ul className="flex">
+        {navLinks.map((route) => (
+          <li key={route.id} className="mr-10">
+            <a href={route.path}>{route.name}</a>
+          </li>
+        ))}
+      </ul> */}
+
+      {/* 1. static navbar */}
+      {/* <ul className="flex">
+        <li className="mr-10">
+          <a href="/">Home</a>
+        </li>
+        <li className="mr-10">
+          <a href="/about">About</a>
+        </li>
+        <li className="mr-10">
+          <a href="/blog">Blog</a>
+        </li>
+      </ul> */}
+
+      <button>Sign In</button>
+    </nav>
+  );
+};
+
+export default NavBar;
